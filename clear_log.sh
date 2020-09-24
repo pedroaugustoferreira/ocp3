930d79bb8aa515a482f374e88e23d64c191649fb
pedroaugustoferreira <43154432+pedroaugustoferreira@users.noreply.github.com>
2020-09-23 21:56:32 -0300

    Create clear_log.sh

---
+++ clear_log.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+cat > /usr/local/bin/clear_json_log.sh << EOF
+#! /bin/bash
+ls  /var/lib/docker/containers/*/*-json.log | awk '{print "> "$0}'|sh 
+EOF
+
+chmod +x /usr/local/bin/clear_json_log.sh
+
+echo "0 5 * * * /usr/local/bin/clear_json_log.sh &> /tmp/clear_json_log.log" >> /var/spool/cron/root
+
+ls -la /usr/local/bin/clear_json_log.sh
+
+crontab -l |grep clear_json_log
